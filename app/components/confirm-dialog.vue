bc6342c1ed36d617917b170daee1730c20eceb70
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-08-24 08:10:31 +0700

    user ok

---
+++ app/components/confirm-dialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <u-modal
+    :open="show"
+    :close="{ onClick: () => emit('cancel', false) }"
+    :title="title"
+    description="Aksi ini tidak bisa dibatalkan"
+  >
+    <template #body>
+      <p class="text-center">{{ message }}</p>
+    </template>
+    <template #footer>
+      <div class="flex justify-center items-center gap-2 w-full">
+        <u-button
+          color="neutral"
+          variant="outline"
+          label="Tidak"
+          @click="emit('cancel', true)"
+        />
+        <u-button color="error" label="Ya" @click="emit('ok', false)" />
+      </div>
+    </template>
+  </u-modal>
+</template>
+
+<script setup>
+const emit = defineEmits(["cancel", "ok"]);
+
+defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: "Konfirmasi",
+  },
+  message: {
+    type: String,
+    default: "Apakah anda yakin?",
+  },
+});
+</script>
--- app/pages/admin/users.vue
+++ app/pages/admin/users.vue
@@ -1,24 +1,65 @@
 <template>
-  <u-card>
-    <h1 class="text-2xl">Kelola Pengguna</h1>
-    <div class="text-sm mb-8">Kelola pengguna aplikasi</div>
+  <h1 class="text-2xl">Kelola Pengguna</h1>
+  <div class="text-sm mb-8">Kelola pengguna aplikasi</div>
 
-    <UTable :data="data?.data ?? []" :columns="columns" class="flex-1">
-      <template #action-cell="{ row }">
-        <UDropdownMenu :items="getDropdownActions(row.original)">
-          <UButton
-            icon="i-lucide-ellipsis-vertical"
-            color="neutral"
-            variant="ghost"
-            aria-label="Actions"
-            size="xs"
-          />
-        </UDropdownMenu>
-      </template>
-    </UTable>
+  <form @submit.prevent class="flex gap-4 mb-6">
+    <u-input
+      type="search"
+      v-model="filter.search"
+      placeholder="Cari pengguna..."
+      class="w-full"
+      icon="i-lucide-search"
+    />
 
-    <div class="flex items-center justify-center my-4">
+    <u-select
+      v-model="filter.role"
+      :items="['SEMUA', 'ADMIN', 'EDITOR', 'USER']"
+      class="min-w-48"
+      placeholder="Filter Role"
+      icon="i-lucide-filter"
+    />
+
+    <u-button
+      type="button"
+      icon="i-lucide-plus"
+      color="warning"
+      variant="solid"
+      @click="createUser"
+      class="min-w-48"
+    >
+      Tambah Pengguna
+    </u-button>
+  </form>
+
+  <UTable
+    :data="data?.data ?? []"
+    :columns="columns"
+    class="flex-1 border-y border-y-gray-200"
+  >
+    <template #action-cell="{ row }">
+      <UDropdownMenu :items="getDropdownActions(row.original)">
+        <UButton
+          icon="i-lucide-ellipsis"
+          color="neutral"
+          variant="ghost"
+          aria-label="Actions"
+          size="xs"
+        />
+      </UDropdownMenu>
+    </template>
+  </UTable>
+
+  <div class="flex flex-wrap gap-4 mt-6 items-center justify-between">
+    <div class="flex flex-wrap gap-2">
+      <u-select
+        v-model="pageSize"
+        size="sm"
+        :items="[10, 25, 50, 100]"
+        class="min-w-16"
+      />
+
       <UPagination
+        size="sm"
         active-color="warning"
         :page="data?.current_page ?? 1"
         :items-per-page="data?.per_page ?? 10"
@@ -26,12 +67,42 @@
         @update:page="(page) => handlePageUpdate(page)"
       />
     </div>
-  </u-card>
+
+    <div>
+      Halaman
+      <u-input
+        v-model="page"
+        type="number"
+        size="sm"
+        min="1"
+        class="mx-2 min-w-16"
+        :max="data?.last_page ?? 1"
+      />
+      dari {{ data?.last_page ?? 1 }}
+    </div>
+
+    <div>
+      Menampilkan {{ data?.from ?? 0 }} - {{ data?.to ?? 0 }} dari
+      {{ data?.total ?? 0 }} data
+    </div>
+  </div>
+
+  <lazy-confirm-dialog
+    :show="showDeleteDialog"
+    title="Hapus Pengguna"
+    :message="`Apakah anda yakin ingin menghapus pengguna '${selectedUser?.name}'?`"
+    @ok="handleDelete(selectedUser?.id)"
+    @cancel="() => (showDeleteDialog = false)"
+  />
 </template>
 
 <script setup lang="ts">
+import { UInput } from "#components";
 import type { DropdownMenuItem, TableColumn } from "@nuxt/ui";
 import type { PaginatedData } from "~/types";
+
+const { $api } = useNuxtApp();
+const toast = useToast();
 
 type User = {
   id: number;
@@ -54,13 +125,54 @@ const columns: TableColumn<User>[] = [
 ];
 
 const page = ref(1);
+const pageSize = ref(10);
+const filter = reactive({
+  role: undefined,
+  search: "",
+});
 
-const { data } = await useApi<PaginatedData<User>>(`/api/user`, {
-  query: computed(() => ({ page: page.value })),
+const showDeleteDialog = ref(false);
+const selectedUser = ref<User | null>(null);
+
+const { data, refresh } = await useApi<PaginatedData<User>>(`/api/user`, {
+  query: computed(() => ({
+    page: page.value,
+    ...filter,
+    pageSize: pageSize.value,
+  })),
 });
 
 function handlePageUpdate(p: number) {
   page.value = p;
+}
+
+function createUser() {}
+
+function openDeleteDialog(user: User) {
+  selectedUser.value = user;
+  showDeleteDialog.value = true;
+}
+
+async function handleDelete(id?: number) {
+  if (!id) return;
+  showDeleteDialog.value = false;
+  try {
+    await $api(`/api/user/${id}`, { method: "DELETE" });
+    toast.add({
+      title: "Sukses",
+      description: "Berhasil menghapus pengguna",
+      color: "success",
+      icon: "i-lucide-check-circle",
+    });
+  } catch (error) {
+    toast.add({
+      title: "Error",
+      description: "Gagal menghapus pengguna",
+      color: "error",
+      icon: "i-lucide-x-circle",
+    });
+  }
+  refresh();
 }
 
 function getDropdownActions(user: User): DropdownMenuItem[][] {
@@ -78,7 +190,7 @@ function getDropdownActions(user: User): DropdownMenuItem[][] {
         icon: "i-lucide-trash",
         color: "error",
         onSelect: () => {
-          console.log(user);
+          openDeleteDialog(user);
         },
       },
     ],
